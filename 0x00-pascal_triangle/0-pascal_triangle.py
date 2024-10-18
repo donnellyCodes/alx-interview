7ccf160793eb8cb3a35135d398d08fb241673813
Donnelly <donamaitsa@gmail.com>
2024-10-18 16:54:18 +0300

    correting code

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,28 +3,21 @@
     Generates a Pascal's triangle of size n.
 """
 def pascal_triangle(n):
-    # if n is less than or equal to 0, return empty list
-    if n <= 0:
-        return []
-    
-    # initialize first row with 1
-    triangle = [[1]]
-    
-    
-    # loop to generate each row of the triangle
-    for i in range(1, n):
-        # start each row with 1
-        row = [1]
-        
-        # loop to calculate each element in the row except the first and last
-        for j in range(1, i):
-            # each value is the sum of the two values directly above it
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        
-        # append the last element of the current row to the triangle
-        row.append(1)
-        
-        # append the current row to the triangle
-        triangle.append(row)
-    # return the completed Pascal's triangle
+    '''Creates a list of lists of integers representing
+    the Pascal's triangle of a given integer.
+    '''
+    # initialize triangle as an empty string
+    triangle = []
+    # checks if n is an integer and if n is less than or equal to 0, returns an empty triangle
+    if type(n) is not int or n <= 0:
+        return triangle
+    # outer loop iterates i and inner loop iterates j
+    for i in range(n):
+        line = []
+        for j in range(i+1):
+            if j == 0 or j == i:
+                line.append(1)
+            elif i > 0 and j < 0:
+                line.append(triangle[i-1][j-1] + triangle[i-1][j])
+        triangle.append(line);
     return triangle
