0352c241d34c54eb5bf60d90c0d54d19da5524c2
Donnelly <donamaitsa@gmail.com>
2024-10-18 17:08:42 +0300

    fixing pep8

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,13 +2,16 @@
 """
     Generates a Pascal's triangle of size n.
 """
+
+
 def pascal_triangle(n):
     '''Creates a list of lists of integers representing
     the Pascal's triangle of a given integer.
     '''
     # initialize triangle as an empty string
     triangle = []
-    # checks if n is an integer and if n is less than or equal to 0, returns an empty triangle
+    # checks if n is an integer and if n 
+    # is less than or equal to 0, returns an empty triangle
     if type(n) is not int or n <= 0:
         return triangle
     # outer loop iterates i and inner loop iterates j
@@ -19,5 +22,5 @@ def pascal_triangle(n):
                 line.append(1)
             elif i > 0 and j > 0:
                 line.append(triangle[i-1][j-1] + triangle[i-1][j])
-        triangle.append(line);
+        triangle.append(line)
     return triangle
